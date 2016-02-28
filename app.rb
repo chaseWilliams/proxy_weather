92b344e6cd25004de9d42dc74c6fca6242ef8381
chaseWilliams <bornofawesomeness@gmail.com>
2016-02-27 23:40:29 -0500

    added redis functionality within Apcera instances

--- app.rb
+++ app.rb
@@ -8,11 +8,11 @@ class App < Sinatra::Application
 
   get '/weather' do
     if params[:mode] = 'zip'
-      weather = Weather_man.get_weather_zip params[:zip_code].to_i
+      weather = Weather_man.weather_is 'zip', params[:zip_code].to_i
       {status: 'ok', data: weather}.to_json
     elsif params[:mode] = 'coordinates'
       zip_code = (GoogleGeocoder.reverse_geocode [params[:lat].to_i, params[:lon].to_i]).zip
-      weather = Weather_man.get_weather_zip zip_code
+      weather = Weather_man.weather_is 'zip', zip_code
       {status: 'ok', data: weather}.to_json
     else
       {status: 'bad_mode'}.to_json
--- weather/weather.rb
+++ weather/weather.rb
@@ -1,41 +1,38 @@
 
 #this module allows for a weather data fetch, supplied via the getWeather method.
 module Weather_man
-  def self.getWeather cityID
-    response = openweathermap "id=#{cityID}"
-    weather_data = {
-      temp: (1.8*(response['main']['temp'].to_f-273)+32).round,
-      cloudiness: response['clouds']['all'].to_f.round,
-      humidity: response['main']['humidity'].to_f.round,
-      windiness: response['wind']['speed'].to_f.round(1),
-      condition_id: response['weather'][0]['id'].to_i,
-      condition_name: response['weather'][0]['main'],
-      condition_description: response['weather'][0]['description'],
-      condition_img: response['weather'][0]['icon']
-    }
-    return weather_data
-  end
+  class Proxy
+    def initialize
+      @redis = Redis.new(url: ENV['REDIS_URI'])
+    end
 
-  def self.get_weather_zip zip_code
-    response = openweathermap "zip=#{zip_code}"
-    weather_data = {
-      temp: (1.8*(response['main']['temp'].to_f-273)+32).round,
-      cloudiness: response['clouds']['all'].to_f.round,
-      humidity: response['main']['humidity'].to_f.round,
-      windiness: response['wind']['speed'].to_f.round(1),
-      condition_id: response['weather'][0]['id'].to_i,
-      condition_name: response['weather'][0]['main'],
-      condition_description: response['weather'][0]['description'],
-      condition_img: response['weather'][0]['icon']
-    }
-    return weather_data
-  end
+    def weather_is mode, geo
+      if @redis.exists('weather_data') == 0
+        mode == zip ? identifier = "zip=#{geo}" : identifer = "id=#{geo}"
+        response = openweathermap identifier
+        weather_data = {
+          temp: (1.8*(response['main']['temp'].to_f-273)+32).round,
+          cloudiness: response['clouds']['all'].to_f.round,
+          humidity: response['main']['humidity'].to_f.round,
+          windiness: response['wind']['speed'].to_f.round(1),
+          condition_id: response['weather'][0]['id'].to_i,
+          condition_name: response['weather'][0]['main'],
+          condition_description: response['weather'][0]['description'],
+          condition_img: response['weather'][0]['icon']
+        }
+        @redis.set 'weather_data', weather_data.to_json
+        @redis.expire 'weather_data', 600
+        return weather_data
+      else
+        @redis.get('weather_data').to_json
+      end
+    end
+    def self.openweathermap params
+      return JSON.parse RestClient.get 'http://api.openweathermap.org/data/2.5/weather?' + params + "&APPID=#{ENV['OWM_KEY']}"
+    end
 
-  def self.openweathermap params
-    return JSON.parse RestClient.get 'http://api.openweathermap.org/data/2.5/weather?' + params + "&APPID=#{ENV['OWM_KEY']}"
-  end
-
-  class << self
-    private :openweathermap
+    class << self
+      private :openweathermap
+    end
   end
 end
