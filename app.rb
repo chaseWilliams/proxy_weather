481e1f02131aed81125679f4c95711ebd880e552
chaseWilliams <bornofawesomeness@gmail.com>
2016-02-29 23:19:19 -0500

    updated routes

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Application
 
   get '/' do
     if params[:mode] == 'zip'
-      weather = @w_proxy.weather_is 'zip', params[:zip_code].to_i
+      weather = @w_proxy.weather_is 'zip', params[:zipcode].to_i
       {status: 'ok', data: weather}.to_json
     elsif params[:mode] == 'coordinates'
       zip_code = (GoogleGeocoder.reverse_geocode [params[:lat].to_i, params[:lon].to_i]).zip
